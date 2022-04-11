e1c1e4083cb41f4a1022412de06bf5459dc950ba
runalsh <shaidullin2@gmail.com>
2022-04-11 15:39:28 +0300

    Create task1.tf

---
+++ tf/1/task1.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-central-2"
+}
+
+data "aws_vpcs" "vpc_main" {
+	default = true
+}
+
+data "aws_subnets" "subnets" {
+}
+
+data "aws_security_groups" "sec_groups" {
+}
+
+output "vpcs" {
+	  description = "vpc id"
+  value = data.aws_vpcs.vpc_main.ids
+} 
+
+output "subnets" {
+  value = data.aws_subnets.subnets.ids
+}
+
+output "secgroups" {
+  value = data.aws_security_groups.sec_groups.ids
+}
